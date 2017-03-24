d705d2ebf0a37dbd00414c4b96942a6802a68130
stefKristijan <stef.kristijan@gmail.com>
2017-03-24 10:14:19 +0100

    readme

---
+++ GitApp/AppForGit/AppForGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije - Kristijan Štefančić D2435");
+           
+        }
+    }
+}
